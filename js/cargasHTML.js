b55320d6b7c5fa82fca259153607297175662fb5
ULISESROSALESALEJOS <urosales507@gmail.com>
2023-05-12 02:03:47 -0400

    Cree la funcion js para cargar HTML sin necesidad de refrescar y actualice Contacto.html

---
+++ js/cargasHTML.js
@@ -0,0 +1,26 @@
+/*FUNCIONES PARA CARGAR HTML*/
+function cargarHTML(pagina){
+    var elemento = document.getElementById("contenedor_inicio");
+    elemento.innerHTML = "";
+
+    var rutaHTML = `../${pagina}.html`
+    console.log(rutaHTML)
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200 && rutaHTML == "../Index.html") {
+            var contenidoHTML = this.responseText;
+            var inicio = document.createElement('div');
+            inicio.innerHTML = contenidoHTML;
+            var contenidoIndex = inicio.querySelector('#contenedor_inicio').innerHTML;
+
+            var contenedor = document.getElementById('contenido');
+            contenedor.innerHTML = contenidoIndex;
+            }
+        else if(this.readyState == 4 && this.status == 200){
+            document.getElementById("contenido").innerHTML = this.responseText;
+        }
+        };
+        xhttp.open("GET",rutaHTML, true);
+        xhttp.send();
+    }
